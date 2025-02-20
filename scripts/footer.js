d878a564d6d2771da52401168fcba569125bf901
Nyktor <nyktorrodriguez@gmail.com>
2025-02-20 17:22:41 +0100

    Modified CSS and index.html

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,1 +1,13 @@
-document.write('')
+document.write('<footer>\
+        <div id="divFooterContenidos">\
+            <a href="index.html"><div>Inicio</div></a>\
+            <a href="componentes.html"><div>Componentes</div></a>\
+            <a href="presupuesto.html"><div>Presupuesto</div></a>\
+            <a href="montaje.html"><div>Montaje</div></a>\
+            <a href="comprobaciones.html"><div>Comprobaciones</div></a>\
+        </div>\
+        \
+        <div id="divFooterDerechos">\
+            Víctor Medrano, 2025 &copy; All rites reversed.\
+        </div>\
+    </footer>')
--- scripts/header.js
+++ scripts/header.js
@@ -1,1 +1,19 @@
-document.write('')
+document.write('<header>\
+        <div id="divHeader">\
+            <div id="divHeaderTitulo">\
+                Montaje de un PC Gamer\
+            </div>\
+            <div id="divHeaderAutor">\
+                <span>Víctor Medrano Rodríguez<br></span>\
+                <span>ASIX 2<br></span>\
+                <span>M5, UF1</span>\
+            </div>\
+        </div>\
+        <div id="divHeaderMenu">\
+            <a href="index.html"><div>Inicio</div></a>\
+            <a href="componentes.html"><div>Componentes</div></a>\
+            <a href="presupuesto.html"><div>Presupuesto</div></a>\
+            <a href="montaje.html"><div>Montaje</div></a>\
+            <a href="comprobaciones.html"><div>Comprobaciones</div></a>\
+        </div>\
+    </header>')
